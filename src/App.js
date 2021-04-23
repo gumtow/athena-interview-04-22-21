7111300e967985e2504e682bcf1ddff32068f48b
jessegumtow <gumtow@gmail.com>
2021-04-22 22:38:27 -0500

    removed console.logs

--- src/App.js
+++ src/App.js
@@ -3,67 +3,53 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
-  const [language, setLanguage] = useState("All")
+  const [language, setLanguage] = useState("All");
 
-  
-  useEffect( () => {
-    
-    async function fetchData(language){
+  useEffect(() => {
+    async function fetchData(language) {
       const body = await fetch(
         "https://api.github.com/search/repositories?q=stars:%3E10000&sort=stars"
       );
       const json = await body.json();
-      console.log(json)
-      console.log(language)
       if (language !== "All") {
         let filterData = json.items.filter((languageFilter) => {
-          return languageFilter.language === language
-        })
+          return languageFilter.language === language;
+        });
         setData(filterData);
       } else {
         setData(json.items);
       }
-      
     }
-    fetchData(language)
-
+    fetchData(language);
   }, [language]);
-
-  // const languageFilter = (language) => {
-      
-  //     let newData = data.filter( (languageFilter)=>{
-  //       return languageFilter.language === language
-        
-  //     });
-  //     console.log('newData', newData);
-
-  //     setData(newData);
-  // }
-  
 
   return (
     <div className="App">
       <div className="container">
         <nav>
           <ul>
-            <li onClick={()=>setLanguage("All")}>All</li>
-            <li onClick={()=>setLanguage("JavaScript")}>JavaScript</li>
-            <li onClick={()=>setLanguage("TypeScript")}>TypeScript</li>
-            <li onClick={()=>setLanguage("Java")}>Java</li>
-            <li  onClick={()=>setLanguage("C++")}>C++</li>
-            <li  onClick={()=>setLanguage("Python")}>Python</li>
+            <li onClick={() => setLanguage("All")}>All</li>
+            <li onClick={() => setLanguage("JavaScript")}>JavaScript</li>
+            <li onClick={() => setLanguage("TypeScript")}>TypeScript</li>
+            <li onClick={() => setLanguage("Java")}>Java</li>
+            <li onClick={() => setLanguage("C++")}>C++</li>
+            <li onClick={() => setLanguage("Python")}>Python</li>
           </ul>
         </nav>
-        {data.map((item, i) => { 
+        {data.map((item, i) => {
           return (
-          <div className="item">
-            <p className="number">#{i + 1}</p>
-            <img className="profile-img" alt={item.name} src={item.owner.avatar_url} />
-            <p className="name">{item.name}</p>
-            <p className="login">{item.owner.login}</p>
-            <p>{item.language}</p>
-          </div>
-        )
+            <div className="item">
+              <p className="number">#{i + 1}</p>
+              <img
+                className="profile-img"
+                alt={item.name}
+                src={item.owner.avatar_url}
+              />
+              <p className="name">{item.name}</p>
+              <p className="login">{item.owner.login}</p>
+              <p>{item.language}</p>
+            </div>
+          );
         })}
       </div>
     </div>
